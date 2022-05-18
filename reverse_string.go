5deb4e9e13a00321b5c108fe1a291ac0554b2dde
Meirbek <53935454+MeirbekAshirbayev@users.noreply.github.com>
2022-05-18 12:07:04 +0600

    Add files via upload

--- reverse_string.go
+++ reverse_string.go
@@ -2,5 +2,16 @@ package reverse_string
 
 func ReverseString(input string) (output string) {
 	// solution goes here
+
+	s1 := ""
+	m := []rune{}
+	for _, v := range input {
+		m = append(m, v)
+	}
+
+	for i := range m {
+		s1 = s1 + string(m[len(m)-1-i])
+	}
+	output = s1
 	return output
 }
